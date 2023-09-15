6079efda024deadc625b2ba0b4a8f8da2c0504a5
Luisestteban <136272246+Luisestteban@users.noreply.github.com>
2023-09-14 23:26:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
-/* Estilos generales para el cuerpo de la página */
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f9f9f9;
-    margin: 0;
-    padding: 0;
-    background-color: white;
-}
+const toggleButtons = document.querySelectorAll('.toggle-answer');
 
-/* Contenedor principal de preguntas frecuentes */
-.faq-container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-    background-color: #fff;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
+toggleButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
 
-/* Estilos para el título */
-h1 {
-    font-size: 28px;
-    text-align: center;
-    margin-bottom: 20px;
-    text-align: center;
-}
+        if (answer.classList.contains('hidden')) {
+            document.querySelectorAll('.answer').forEach(a => {
+                a.classList.add('hidden');
+            });
 
-/* Estilos para las preguntas */
-.question {
-    font-size: 20px;
-    color: #333;
-    cursor: pointer;
-    margin: 0;
-    padding: 10px 0;
-    border-bottom: 1px solid #ccc;
-}
+            answer.classList.remove('hidden');
+            toggleButtons.forEach(btn => {
+                if (btn !== button) {
+                    btn.textContent = 'Mostrar respuesta';
+                } else {
+                    button.textContent = 'Ocultar respuesta';
+                }
+            });
+        } else {
 
-/* Estilos para los botones de mostrar/ocultar respuestas */
-.toggle-answer {
-    background-color: #007BFF;
-    color: #fff;
-    border: none;
-    padding: 10px 20px;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-top: 10px;
-}
+            answer.classList.add('hidden');
 
-/* Estilos para las respuestas (ocultas por defecto) */
-.answer {
-    display: none;
-    font-size: 16px;
-    margin-top: 10px;
-}
-
-/* Estilos para las respuestas cuando se muestran */
-.answer:not(.hidden) {
-    display: block;
-}
+            button.textContent = 'Mostrar respuesta';
+        }
+    });
+});
