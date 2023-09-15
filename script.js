a4954ec2ea8f1a498edb4a5141e0552670cf5f51
Luisestteban <136272246+Luisestteban@users.noreply.github.com>
2023-09-14 23:09:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const questions = document.querySelectorAll('.question');
+// Obtén todos los elementos de clase "toggle-answer"
+const toggleButtons = document.querySelectorAll('.toggle-answer');
 
-questions.forEach((question) => {
-    question.addEventListener('click', () => {
-        const answer = question.nextElementSibling;
-        if (answer.style.display === 'block') {
-            answer.style.display = 'none';
+// Agrega un evento clic a cada botón
+toggleButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Encuentra el elemento "p" (respuesta) hermano del botón actual
+        const answer = button.nextElementSibling;
+
+        // Verifica si la respuesta está oculta
+        if (answer.classList.contains('hidden')) {
+            // Cierra todas las respuestas abiertas
+            document.querySelectorAll('.answer').forEach(a => {
+                a.classList.add('hidden');
+            });
+
+            // Muestra la respuesta actual
+            answer.classList.remove('hidden');
+            
+            // Cambia el texto del botón a "Ocultar respuesta" solo para el botón actual
+            toggleButtons.forEach(btn => {
+                if (btn !== button) {
+                    btn.textContent = 'Mostrar respuesta';
+                } else {
+                    button.textContent = 'Ocultar respuesta';
+                }
+            });
         } else {
-            answer.style.display = 'block';
+            // Oculta la respuesta actual
+            answer.classList.add('hidden');
+
+            // Cambia el texto del botón a "Mostrar respuesta" solo para el botón actual
+            button.textContent = 'Mostrar respuesta';
         }
     });
 });
